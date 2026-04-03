f10b238c241fdef25a070a5c0c4bae18fdb4557b
Tiago de Oliveira Silva <tiagodeoliveirasilva@MacBook-Air-de-Tiago.local>
2026-04-03 10:22:09 -0300

    Ajuste dos nomes do Agente new

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -6,7 +6,7 @@ import ErrorMessage from '../components/ErrorMessage';
 import useChat from '../hooks/useChat';
 
 const APP_TITLE = import.meta.env.VITE_APP_TITLE || 'Chat com IA';
-const APP_SUBTITLE = import.meta.env.VITE_APP_SUBTITLE || 'Assistente inteligente do SEPLAN-PI';
+const APP_SUBTITLE = import.meta.env.VITE_APP_SUBTITLE || 'Assistente inteligente do Projeto Pilares 2';
 const WELCOME_TITLE = import.meta.env.VITE_WELCOME_TITLE || 'Bem-vindo ao Chat com Agente Virtual do Projeto Pilares 2';
 const WELCOME_MESSAGE = import.meta.env.VITE_WELCOME_MESSAGE || 'Envie sua primeira mensagem para começar a conversar com o assistente inteligente.';
 const TYPING_MESSAGE = import.meta.env.VITE_TYPING_MESSAGE || 'Assistente está digitando...';
